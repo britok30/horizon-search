3387159e92af53f3f9532ca99c4b4154375a5f09
Kelvin Brito <britok30@gmail.com>
2021-11-11 18:55:18 -0500

    finish dark mode implementation + create search component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Search } from './Search';
 
 export const Navbar = ({ darkTheme, setDarkTheme }) => {
     return (
-        <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
+        <div className="p-5 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
             <div className="flex justify-between items-center space-x-5 w-screen">
                 <Link to="/">
                     <p className="text-2xl font-bold text-white py-1 px-2 bg-blue-500 rounded dark:bg-gray-500 dark:text-gray-900">
@@ -12,11 +13,12 @@ export const Navbar = ({ darkTheme, setDarkTheme }) => {
                 </Link>
                 <button
                     type="button"
-                    className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border w-20 rounded-md px-2 py-1 hover:shadow-lg transition-all duration-300 ease-in-out"
+                    className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border w-24 rounded-md px-2 py-1 hover:shadow-lg transition-all duration-300 ease-in-out flex justify-center items-center"
                     onClick={() => setDarkTheme(!darkTheme)}
                 >
-                    {darkTheme ? 'Light' : 'Dark'}
+                    {darkTheme ? 'Light 💡' : 'Dark 🌙'}
                 </button>
+                <Search />
             </div>
         </div>
     );
