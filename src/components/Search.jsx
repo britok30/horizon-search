b04b89ef0bc715de82512723e01450f44123aa4f
Kelvin Brito <britok30@gmail.com>
2021-11-11 22:14:48 -0500

    proper fixes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useResultContext } from '../contexts/ResultContextProvider';
+import Links from './Links';
 
 export const Search = () => {
+    const { searchTerm } = useResultContext();
     return (
-        <div>
+        <div className="mt-4 w-full flex flex-col items-center">
             <input
                 type="text"
-                className="p-2 rounded-md focus:outline-none dark:bg-gray-500"
+                className="p-2 rounded-md focus:outline-none dark:bg-gray-500 w-1/2"
                 placeholder="Search..."
+                onChange={(e) => e.target.value}
             />
+            <Links />
         </div>
     );
 };
--- src/components/VideoResult.jsx
+++ src/components/VideoResult.jsx
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player';
 const VideoResult = ({ video }) => {
     return (
         <ReactPlayer
-            url={video.additional_links[0].href}
+            url={video.additional_links?.[0].href}
             controls
             width="355px"
             height="200px"
