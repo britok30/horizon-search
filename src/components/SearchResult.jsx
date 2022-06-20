170b2e6bf8e7db773ee8b2e2d554c7f88e0b7c41
Kelvin Brito <britok30@gmail.com>
2022-06-20 14:17:17 -0400

    add cursor pointer

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -5,7 +5,12 @@ export const SearchResult = (result) => {
 
   return (
     <div className="md:w-2/5 w-full">
-      <a href={searchResult.link} target="_blank" rel="noreferrer">
+      <a
+        className="cursor-pointer"
+        href={searchResult.link}
+        target="_blank"
+        rel="noreferrer"
+      >
         <span className="text-lg hover:underline dark:text-blue-300 text-blue-700">
           {searchResult.title}
         </span>
